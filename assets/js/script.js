12a2c277f12e0a5c68e61222994bc8bd77d8c5dd
jbeedle19 <josh.beedle@gmail.com>
2020-11-30 14:30:06 -0500

    tested search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
 // Variables:
-var searchTerm = $("")
+
+
 
 // Functions:
 
 // Event Listeners:
+// Placeholder listener to test Search Field
+$("#search-btn").on("click", function(event) {
+    event.preventDefault();
+    var searchTerm = $("#search-term").val();
+    console.log(searchTerm);
+    var timeframe = $("#timeframe").val();
+    console.log(timeframe)
+})
