8461d3ede949c6b598f79bd1b52f0b676d2e94c3
Mcrouse <matthew.crouse@temple.edu>
2020-11-30 17:57:28 -0500

    updated search feature. shows predicited country/cities but doesn't link to map yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ var map, infoWindow;
 
 // Functions:
 
+// Creating map
 function createMap() {
     var options = {
         center: { lat: 39.9526, lng: -75.1652},
@@ -12,7 +13,7 @@ function createMap() {
     };
 
     map = new google.maps.Map(document.getElementById('map'), options);
-
+    // Centers map for new users to their current location
     infoWindow = new google.maps.InfoWindow;
 
     if (navigator.geolocation) {
@@ -31,8 +32,53 @@ function createMap() {
     } else {
         handleLocationError('No geolocation available', map.center());
     } 
+    
+    //Search for new places 
+    var input = document.getElementById('search-term');
+    var searchBox = new google.maps.places.SearchBox(input);
+
+    //Looks for areas close to their location first 
+    map.addListener('bounds_changed', function () {
+        searchBox.setBounds(map.getBounds());
+    });
+    
+    //shows search results on map
+    var markers = [];
+    
+    //Selects places predicted from searchbox
+    searchBox.addListener('places_changes', function () {
+        var places = searchBox.getPlaces();
+
+        if (places.length === 0)
+            return;
+         
+         //clears out previous markers
+        markers.forEach(function (m) { m.setMap(null); });
+        markers = [];
+
+        //Coordinate boundaries of map
+        var bounds = new google.maps.LatLngBounds();
+        
+        places.forEach(function (p) {
+            if (!p.geometry)
+                return;
+            
+            markers.push(new google.maps.Marker({
+                map: map,
+                title: p.name,
+                position: p.geometry.location
+            }));
+
+            if (p.geometry.viewport)
+                bounds.union(p.geometry.viewport);
+            else
+                bounds.extend(p.geometry.location);
+        });
+        map.fitBounds(bounds);
+    });
 }
 
+// Location error function 
 function handleLocationError(content, position) {
     infoWindow.setPosition(position); 
     infowWindow.setContent(content);
