b8abb0a68b3f0bec52952d98286836f840a1b529
Mcrouse <matthew.crouse@temple.edu>
2020-11-30 17:06:52 -0500

    updated map to center on current location, if your location is not enabled it will go to default

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Variables:
-var map;
+var map, infoWindow;
 
 
 
@@ -12,6 +12,31 @@ function createMap() {
     };
 
     map = new google.maps.Map(document.getElementById('map'), options);
+
+    infoWindow = new google.maps.InfoWindow;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (p) {
+            var position = {
+                lat: p.coords.latitude,
+                lng: p.coords.longitutde
+            };
+            infoWindow.setPosition(position);
+            infoWindow.setContent('Your location');
+            infoWindow.open(map);
+        }, function () {
+            handleLocationError('Geolocation service failed', map.center());
+        })
+            
+    } else {
+        handleLocationError('No geolocation available', map.center());
+    } 
+}
+
+function handleLocationError(content, position) {
+    infoWindow.setPosition(position); 
+    infowWindow.setContent(content);
+    infoWindow.open(map);
 }
 
 
