b6b59df782faeb28917b4cb13a783cf5fdec17ee
Mcrouse <matthew.crouse@temple.edu>
2020-12-01 22:59:50 -0500

    rearranged some of the old code and put the predicition function into the new map function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,93 +2,6 @@
 // var map, infoWindow;
 
 
-
-// // Functions:
-
-// // Creating map
-// function createMap() {
-//     var options = {
-//         center: { lat: 39.9526, lng: -75.1652},
-//         zoom: 10
-//     };
-
-//     map = new google.maps.Map(document.getElementById('map'), options);
-    
-//     // Centers map for new users to their current location
-//     infoWindow = new google.maps.InfoWindow;
-
-//     if (navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition(function (p) {
-//             var position = {
-//                 lat: p.coords.latitude,
-//                 lng: p.coords.longitutde
-//             };
-//             infoWindow.setPosition(position);
-//             infoWindow.setContent('Your location');
-//             infoWindow.open(map);
-//         }, function () {
-//             handleLocationError('Geolocation service failed', map.center());
-//         })
-            
-//     } else {
-//         handleLocationError('No geolocation available', map.center());
-//     } 
-    
-//     //Search for new places 
-//     var input = document.getElementById('search-term');
-//     var searchBox = new google.maps.places.SearchBox(input);
-
-//     //Looks for areas close to their location first 
-//     map.addListener('bounds_changed', function () {
-//         searchBox.setBounds(map.getBounds());
-//     });
-    
-//     //shows search results on map
-//     var markers = [];
-    
-//     //Selects places predicted from searchbox
-//     searchBox.addListener('places_changes', function () {
-//         var places = searchBox.getPlaces();
-
-//         if (places.length === 0)
-//             return;
-         
-//          //clears out previous markers
-//         markers.forEach(function (m) { m.setMap(null); });
-//         markers = [];
-
-//         //Coordinate boundaries of map
-//         var bounds = new google.maps.LatLngBounds();
-        
-//         places.forEach(function (p) {
-//             if (!p.geometry)
-//                 return;
-            
-//             markers.push(new google.maps.Marker({
-//                 map: map,
-//                 title: p.name,
-//                 position: p.geometry.location
-//             }));
-
-//             if (p.geometry.viewport)
-//                 bounds.union(p.geometry.viewport);
-//             else
-//                 bounds.extend(p.geometry.location);
-//         });
-//         map.fitBounds(bounds);
-//     });
-
-
-// }
-
-// // // Location error function 
-// function handleLocationError(content, position) {
-//     infoWindow.setPosition(position); 
-//     infowWindow.setContent(content);
-//     infoWindow.open(map);
-// }
-
-
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
@@ -98,6 +11,76 @@ function initMap() {
   document.getElementById("search-btn").addEventListener("click", () => {
     geocodeAddress(geocoder, map);
   });
+    // Centers map for new users to their current location
+    infoWindow = new google.maps.InfoWindow;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (p) {
+            var position = {
+                lat: p.coords.latitude,
+                lng: p.coords.longitutde
+            };
+            infoWindow.setPosition(position);
+            infoWindow.setContent('Your location');
+            infoWindow.open(map);
+        }, function () {
+            handleLocationError('Geolocation service failed', map.center());
+        })
+            
+    } else {
+        handleLocationError('No geolocation available', map.center());
+    } 
+    
+    //Search for new places 
+    var input = document.getElementById('search-term');
+    var searchBox = new google.maps.places.SearchBox(input);
+
+    //Looks for areas close to their location first 
+    map.addListener('bounds_changed', function () {
+        searchBox.setBounds(map.getBounds());
+    });
+    
+    //shows search results on map
+    var markers = [];
+    
+    //Selects places predicted from searchbox
+    searchBox.addListener('places_changes', function () {
+        var places = searchBox.getPlaces();
+
+        if (places.length === 0)
+            return;
+         
+         //clears out previous markers
+        markers.forEach(function (m) { m.setMap(null); });
+        markers = [];
+
+        //Coordinate boundaries of map
+        var bounds = new google.maps.LatLngBounds();
+        
+        places.forEach(function (p) {
+            if (!p.geometry)
+                return;
+            
+            markers.push(new google.maps.Marker({
+                map: map,
+                title: p.name,
+                position: p.geometry.location
+            }));
+
+            if (p.geometry.viewport)
+                bounds.union(p.geometry.viewport);
+            else
+                bounds.extend(p.geometry.location);
+        });
+        map.fitBounds(bounds);
+    });
+}
+
+// // // Location error function 
+function handleLocationError(content, position) {
+    infoWindow.setPosition(position); 
+    infowWindow.setContent(content);
+    infoWindow.open(map);
 }
 
 function geocodeAddress(geocoder, resultsMap) {
