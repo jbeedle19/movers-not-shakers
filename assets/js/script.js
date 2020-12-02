b39d581e08087625bb49a063c166a544e97c42eb
Mcrouse <matthew.crouse@temple.edu>
2020-12-01 22:48:59 -0500

    updated the map, now able to search for a city/country and it loads on the map. But the prediction in the search is no longer functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,97 +1,128 @@
-// Variables:
-var map, infoWindow;
+// // Variables:
+// var map, infoWindow;
 
 
 
-// Functions:
+// // Functions:
 
-// Creating map
-function createMap() {
-    var options = {
-        center: { lat: 39.9526, lng: -75.1652},
-        zoom: 10
-    };
+// // Creating map
+// function createMap() {
+//     var options = {
+//         center: { lat: 39.9526, lng: -75.1652},
+//         zoom: 10
+//     };
 
-    map = new google.maps.Map(document.getElementById('map'), options);
-    // Centers map for new users to their current location
-    infoWindow = new google.maps.InfoWindow;
+//     map = new google.maps.Map(document.getElementById('map'), options);
+    
+//     // Centers map for new users to their current location
+//     infoWindow = new google.maps.InfoWindow;
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (p) {
-            var position = {
-                lat: p.coords.latitude,
-                lng: p.coords.longitutde
-            };
-            infoWindow.setPosition(position);
-            infoWindow.setContent('Your location');
-            infoWindow.open(map);
-        }, function () {
-            handleLocationError('Geolocation service failed', map.center());
-        })
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(function (p) {
+//             var position = {
+//                 lat: p.coords.latitude,
+//                 lng: p.coords.longitutde
+//             };
+//             infoWindow.setPosition(position);
+//             infoWindow.setContent('Your location');
+//             infoWindow.open(map);
+//         }, function () {
+//             handleLocationError('Geolocation service failed', map.center());
+//         })
             
-    } else {
-        handleLocationError('No geolocation available', map.center());
-    } 
+//     } else {
+//         handleLocationError('No geolocation available', map.center());
+//     } 
     
-    //Search for new places 
-    var input = document.getElementById('search-term');
-    var searchBox = new google.maps.places.SearchBox(input);
+//     //Search for new places 
+//     var input = document.getElementById('search-term');
+//     var searchBox = new google.maps.places.SearchBox(input);
 
-    //Looks for areas close to their location first 
-    map.addListener('bounds_changed', function () {
-        searchBox.setBounds(map.getBounds());
-    });
+//     //Looks for areas close to their location first 
+//     map.addListener('bounds_changed', function () {
+//         searchBox.setBounds(map.getBounds());
+//     });
     
-    //shows search results on map
-    var markers = [];
+//     //shows search results on map
+//     var markers = [];
     
-    //Selects places predicted from searchbox
-    searchBox.addListener('places_changes', function () {
-        var places = searchBox.getPlaces();
+//     //Selects places predicted from searchbox
+//     searchBox.addListener('places_changes', function () {
+//         var places = searchBox.getPlaces();
 
-        if (places.length === 0)
-            return;
+//         if (places.length === 0)
+//             return;
          
-         //clears out previous markers
-        markers.forEach(function (m) { m.setMap(null); });
-        markers = [];
+//          //clears out previous markers
+//         markers.forEach(function (m) { m.setMap(null); });
+//         markers = [];
 
-        //Coordinate boundaries of map
-        var bounds = new google.maps.LatLngBounds();
+//         //Coordinate boundaries of map
+//         var bounds = new google.maps.LatLngBounds();
         
-        places.forEach(function (p) {
-            if (!p.geometry)
-                return;
+//         places.forEach(function (p) {
+//             if (!p.geometry)
+//                 return;
             
-            markers.push(new google.maps.Marker({
-                map: map,
-                title: p.name,
-                position: p.geometry.location
-            }));
+//             markers.push(new google.maps.Marker({
+//                 map: map,
+//                 title: p.name,
+//                 position: p.geometry.location
+//             }));
 
-            if (p.geometry.viewport)
-                bounds.union(p.geometry.viewport);
-            else
-                bounds.extend(p.geometry.location);
-        });
-        map.fitBounds(bounds);
-    });
+//             if (p.geometry.viewport)
+//                 bounds.union(p.geometry.viewport);
+//             else
+//                 bounds.extend(p.geometry.location);
+//         });
+//         map.fitBounds(bounds);
+//     });
+
+
+// }
+
+// // // Location error function 
+// function handleLocationError(content, position) {
+//     infoWindow.setPosition(position); 
+//     infowWindow.setContent(content);
+//     infoWindow.open(map);
+// }
+
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 8,
+    center: { lat: 39.9526, lng: -75.1652 },
+  });
+  const geocoder = new google.maps.Geocoder();
+  document.getElementById("search-btn").addEventListener("click", () => {
+    geocodeAddress(geocoder, map);
+  });
 }
 
-// Location error function 
-function handleLocationError(content, position) {
-    infoWindow.setPosition(position); 
-    infowWindow.setContent(content);
-    infoWindow.open(map);
+function geocodeAddress(geocoder, resultsMap) {
+  const address = document.getElementById("search-term").value;
+  geocoder.geocode({ address: address }, (results, status) => {
+    if (status === "OK") {
+      resultsMap.setCenter(results[0].geometry.location);
+      new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location,
+      });
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+    
 }
 
 
 // Event Listeners:
-// Placeholder listener to test Search Field
+// // Placeholder listener to test Search Field
 $("#search-btn").on("click", function(event) {
     event.preventDefault();
     var searchTerm = $("#search-term").val();
-    console.log(searchTerm);
+    // console.log(searchTerm);
     var timeframe = $(".option").data("timeframe")
-    console.log(timeframe)
+    // console.log(timeframe)
 })
