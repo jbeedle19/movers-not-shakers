851701e6b431e347c986adef8f34041dacf762e8
jbeedle19 <josh.beedle@gmail.com>
2020-12-09 22:00:55 -0500

    adding earthquake layer

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ var lastSearch = localStorage.getItem("search") || '';
 
 function initMap() {
   // Styles a map in night mode.
-  const map = new google.maps.Map(document.getElementById("map"), {
+  map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 40.674, lng: -73.945 },
     zoom: 12,
     styles: [
@@ -93,66 +93,83 @@ function initMap() {
     ],
   });
 
-  
-  
-    const geocoder = new google.maps.Geocoder();
+  const geocoder = new google.maps.Geocoder();
   $("#search-btn").on("click", () => {
     geocodeAddress(geocoder, map);
   });
-  //     // Centers map for new users to their current location
-  //   infoWindow = new google.maps.InfoWindow;
 
-      //Search for new places 
-    var input = document.getElementById('search-term');
-    var searchBox = new google.maps.places.SearchBox(input);
+  const script = document.createElement("script");
+  script.setAttribute(
+    "src",
+    "https://storage.googleapis.com/mapsdevsite/json/quakes.geo.json"
+  );
+  document.getElementsByTagName("head")[0].appendChild(script);
+  // Add a basic style.
+  map.data.setStyle((feature) => {
+    const mag = Math.exp(parseFloat(feature.getProperty("mag"))) * 0.1;
+    return /** @type {google.maps.Data.StyleOptions} */ {
+      icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: mag,
+        fillColor: "#f00",
+        fillOpacity: 0.35,
+        strokeWeight: 0,
+      },
+    };
+  });
 
-    //Looks for areas close to their location first 
-    map.addListener('bounds_changed', function () {
-        searchBox.setBounds(map.getBounds());
+  //Search for new places 
+  var input = document.getElementById('search-term');
+  var searchBox = new google.maps.places.SearchBox(input);
+
+  //Looks for areas close to their location first 
+  map.addListener('bounds_changed', function () {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  //shows search results on map
+  var markers = [];
+
+  //Selects places predicted from searchbox
+  searchBox.addListener('places_changes', function () {
+    var places = searchBox.getPlaces();
+
+    if (places.length === 0)
+      return;
+
+    //clears out previous markers
+    markers.forEach(function (m) { m.setMap(null); });
+    markers = [];
+
+    //Coordinate boundaries of map
+    var bounds = new google.maps.LatLngBounds();
+
+    places.forEach(function (p) {
+      if (!p.geometry)
+        return;
+
+      markers.push(new google.maps.Marker({
+        map: map,
+        title: p.name,
+        position: p.geometry.location
+      }));
+
+      if (p.geometry.viewport)
+        bounds.union(p.geometry.viewport);
+      else
+        bounds.extend(p.geometry.location);
     });
-    
-    //shows search results on map
-    var markers = [];
-    
-    //Selects places predicted from searchbox
-    searchBox.addListener('places_changes', function () {
-        var places = searchBox.getPlaces();
+    map.fitBounds(bounds);
+  });
 
-        if (places.length === 0)
-            return;
-         
-         //clears out previous markers
-        markers.forEach(function (m) { m.setMap(null); });
-        markers = [];
-
-        //Coordinate boundaries of map
-        var bounds = new google.maps.LatLngBounds();
-        
-        places.forEach(function (p) {
-            if (!p.geometry)
-                return;
-            
-            markers.push(new google.maps.Marker({
-                map: map,
-                title: p.name,
-                position: p.geometry.location
-            }));
-
-            if (p.geometry.viewport)
-                bounds.union(p.geometry.viewport);
-            else
-                bounds.extend(p.geometry.location);
-        });
-        map.fitBounds(bounds);
-    });
-  
 }
+
 
 // // // Location error function 
 function handleLocationError(content, position) {
-    infoWindow.setPosition(position); 
-    infowWindow.setContent(content);
-    infoWindow.open(map);
+  infoWindow.setPosition(position);
+  infowWindow.setContent(content);
+  infoWindow.open(map);
 }
 
 function geocodeAddress(geocoder, resultsMap) {
@@ -165,43 +182,43 @@ function geocodeAddress(geocoder, resultsMap) {
         position: results[0].geometry.location,
       });
     } else {
-        $("html").addClass("is-clipped");
-        $("#error-modal").addClass("is-active");
-        $("#error").text("Geocode was not successful for the following reason: " + status);
+      $("html").addClass("is-clipped");
+      $("#error-modal").addClass("is-active");
+      $("#error").text("Geocode was not successful for the following reason: " + status);
     }
   });
-    
+
 }
 
 //Function for displaying Current Weather
 function currentWeather(city) {
-  var apiURLCurrent =  "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + weatherApiKey;
+  var apiURLCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + weatherApiKey;
   fetch(apiURLCurrent)
-      .then(function(response) {
-          if (response.ok) {
-              response.json().then(function(response) {
-              var iconCode = response.weather[0].icon;
-              var iconURL = "https://openweathermap.org/img/wn/" + iconCode + ".png";
-              var currentTemp = response.main.temp + "°F";
-              var iconTempHTML = '<p class="subtitle" id="icon-container"><img id="weatherIcon" src="' + iconURL + '"/></p>' +
-                                 '<p class="subtitle" id="temp">' + currentTemp + '</p>'; 
-                  $('#icon-container').remove();
-                  $('#temp').remove();
-                  $("#weather-container").append(iconTempHTML);
-          });
-          } else {
-              $("html").addClass("is-clipped");
-              $("#error-modal").addClass("is-active");
-              $("#error").text("Not able to find weather for specific location. Please enter city, state or country.");
-              
-          }
-      })
-      .catch(function(error) {
-          $("html").addClass("is-clipped");
-          $("#error-modal").addClass("is-active");
-          $("#error").text("Unable to connect to Weather Report");
-         
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (response) {
+          var iconCode = response.weather[0].icon;
+          var iconURL = "https://openweathermap.org/img/wn/" + iconCode + ".png";
+          var currentTemp = response.main.temp + "°F";
+          var iconTempHTML = '<p class="subtitle" id="icon-container"><img id="weatherIcon" src="' + iconURL + '"/></p>' +
+            '<p class="subtitle" id="temp">' + currentTemp + '</p>';
+          $('#icon-container').remove();
+          $('#temp').remove();
+          $("#weather-container").append(iconTempHTML);
         });
+      } else {
+        $("html").addClass("is-clipped");
+        $("#error-modal").addClass("is-active");
+        $("#error").text("Not able to find weather for specific location. Please enter city, state or country.");
+
+      }
+    })
+    .catch(function (error) {
+      $("html").addClass("is-clipped");
+      $("#error-modal").addClass("is-active");
+      $("#error").text("Unable to connect to Weather Report");
+
+    });
 };
 
 // Function to load anything that was saved in localStorage
@@ -211,23 +228,25 @@ function loadStorage() {
   } else {
     $("#search-term").val(lastSearch);
     currentWeather(lastSearch);
-    setTimeout(function(){ $('#search-btn').click()}, 100);
-  } 
+    setTimeout(function () { $('#search-btn').click() }, 100);
+  }
 }
-loadStorage();
+
 
 // Event Listeners:
 // Listens for search to be clicked and runs currentWeather
-$("#search-btn").on("click", function(event) {
-    event.preventDefault();
-    var searchTerm = $("#search-term").val();
-    localStorage.setItem("search", searchTerm);
-    currentWeather(searchTerm);
+$("#search-btn").on("click", function (event) {
+  event.preventDefault();
+  var searchTerm = $("#search-term").val();
+  localStorage.setItem("search", searchTerm);
+  currentWeather(searchTerm);
 })
 // closes error modal 
 $("#close").on("click", function (event) {
-      event.preventDefault();
-      $("html").removeClass("is-clipped");
-      $("#error-modal").removeClass("is-active");
- });
+  event.preventDefault();
+  $("html").removeClass("is-clipped");
+  $("#error-modal").removeClass("is-active");
+});
 
+loadStorage();
+initMap();
